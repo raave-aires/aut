deb261cd4cbf217aa0edd15f20695f5b3c2329a6
Raave Aires <dev@raave.me>
2025-06-09 11:52:44 -0300

    feat[auth]: redireciona usuários não autenticados para a página de login e melhora a estrutura do componente

--- app/conta/entrou/page.tsx
+++ app/conta/entrou/page.tsx
@@ -1,9 +1,19 @@
 import { auth } from "@/auth"
+import { redirect } from "next/navigation";
+
+import LogoutButton from "@/components/auth/buttons/logout-button";
 
 export default async function(){
   const session = await auth();
 
+  if(!session){
+    redirect('/conta/entrar')
+  };
+
   return(
-    <p>Bem-vindo, {session?.user?.name}!</p>
+    <div className="min-w-dvh min-h-dvh flex flex-col justify-center items-center gap-4">
+      <p>Bem-vindo, {session?.user?.name}!</p>
+      <LogoutButton />
+    </div>
   );
 };
