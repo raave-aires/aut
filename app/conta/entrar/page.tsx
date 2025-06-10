716cb20ddc09082b8254e62f742d87435fcd8fd2
Raave Aires <dev@raave.me>
2025-06-09 23:20:15 -0300

    feat: refatora página de login para incluir AuthHeader, AuthButtons e AuthFooter

--- app/conta/entrar/page.tsx
+++ app/conta/entrar/page.tsx
@@ -1,41 +1,23 @@
-// dependências:
-import React from "react";
-import { LoginButton } from "@/components/auth/buttons/login-button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { LoginForm } from "./form";
-import Link from "next/link";
+// componentes:
+import { Card, CardContent } from "@/components/ui/card";
+import { AuthHeader } from "@/components/auth/auth-header";
+import { AuthButtons } from "@/components/auth/auth-buttons";
+import { LoginForm } from "@/components/auth/forms/login-form";
+import { AuthFooter } from "@/components/auth/auth-footer";
 
 export default function Home() {
   return (
     <main className="min-w-dvw min-h-dvh flex justify-center items-center">
       <Card>
-        <CardHeader>
-          <CardTitle>
-            <span className="font-averia text-4xl">raavë</span>
-          </CardTitle>
-          <CardDescription>
-            Entre para continuar
-          </CardDescription>
-        </CardHeader>
+        <AuthHeader auth_type="login"/>
 
-        <CardContent className="">
-          <div className="grid grid-cols-2 gap-x-4">
-            <LoginButton provider="github"/>
-            <LoginButton provider="google"/>
-          </div>
+        <CardContent>
+          <AuthButtons />
 
-          <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border">ou</p>
           <LoginForm />
         </CardContent>
 
-        <CardFooter className="w-full flex justify-center pt-2">
-          <Link 
-            href="/conta/criar"
-            className="group/linkCriacao"
-          >
-            Não tem uma conta? <span className="underline-offset-4 group-hover/linkCriacao:underline">Crie uma</span>
-          </Link>
-        </CardFooter>
+        <AuthFooter auth_type="login" />
       </Card>
     </main>
   );
