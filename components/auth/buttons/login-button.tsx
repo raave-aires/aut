6a78684af276d953a8664bb127f6fc8b394f8621
Raave Aires <dev@raave.me>
2025-06-09 22:46:38 -0300

    feat: refatora LoginButton para gerenciar estado de carregamento e melhorar a experiência do usuário

--- components/auth/buttons/login-button.tsx
+++ components/auth/buttons/login-button.tsx
@@ -1,9 +1,12 @@
-// dependências:
-import React from "react";
+"use client";
+
+// dependências
+import React, { useState } from "react";
 import { signIn } from "next-auth/react";
 
 // componentes:
 import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/auth/loader";
 
 // ícones:
 import { SiGithub, SiGoogle } from "@icons-pack/react-simple-icons";
@@ -14,13 +17,35 @@ interface LoginButtonProps {
 };
 
 export function LoginButton({ provider, className }: LoginButtonProps) {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      await signIn(provider, { redirectTo: "/conta/entrou" });
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  // define o nome do provider
+  const providerName = provider === "github" ? "GitHub" : "Google";
+
   return (
     <Button
       variant="outline"
       className={className ? `hover:cursor-pointer ${className}` : "hover:cursor-pointer"}
-      onClick={() => signIn(provider, { redirectTo: '/conta/entrou' })}
+      onClick={handleClick}
+      disabled={loading}
     >
-      { provider === "github" ? <><SiGithub /> GitHub</> : <><SiGoogle /> Google</>}
+      { loading ? (
+        <Loader />
+      ) : provider === "github" ? (
+        <SiGithub />
+      ) : (
+        <SiGoogle />
+      )}
+      {providerName}
     </Button>
   );
-};
+}
