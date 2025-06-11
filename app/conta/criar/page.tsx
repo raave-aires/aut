6659f793aa1583784dc821bbf9092b2b72e52edb
Raave Aires <dev@raave.me>
2025-06-11 10:50:15 -0300

    refactor: remove componente Card e simplifica a estrutura do layout de autenticação

--- app/conta/criar/page.tsx
+++ app/conta/criar/page.tsx
@@ -1,24 +1,21 @@
 // componentes:
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { AuthHeader } from "@/components/auth/auth-header";
 import { AuthButtons } from "@/components/auth/auth-buttons";
 import { RegisterForm } from "@/components/auth/forms/register-form";
 import { AuthFooter } from "@/components/auth/auth-footer";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="min-w-dvw min-h-dvh flex justify-center items-center">
-      <Card>
-        <AuthHeader auth_type="register"/>
+    <>
+      <AuthHeader auth_type="register"/>
 
-        <CardContent>
-          <AuthButtons />
+      <CardContent>
+        <AuthButtons />
+        <RegisterForm />
+      </CardContent>
 
-          <RegisterForm />
-        </CardContent>
-
-        <AuthFooter auth_type="register" />
-      </Card>
-    </main>
+      <AuthFooter auth_type="register" />
+    </>
   );
-}
+};
--- app/conta/entrar/page.tsx
+++ app/conta/entrar/page.tsx
@@ -1,25 +1,22 @@
 // componentes:
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { AuthHeader } from "@/components/auth/auth-header";
 import { AuthButtons } from "@/components/auth/auth-buttons";
 import { LoginForm } from "@/components/auth/forms/login-form";
 import { AuthFooter } from "@/components/auth/auth-footer";
-import { PasskeyLoginButton } from "@/components/auth/buttons/passkey-login-button";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="min-w-dvw min-h-dvh flex justify-center items-center">
-      <Card>
-        <AuthHeader auth_type="login"/>
+    <>
+      <AuthHeader auth_type="login" />
 
-        <CardContent>
-          <AuthButtons />
+      <CardContent>
+        <AuthButtons />
 
-          <LoginForm />
-        </CardContent>
+        <LoginForm />
+      </CardContent>
 
-        <AuthFooter auth_type="login" />
-      </Card>
-    </main>
+      <AuthFooter auth_type="login" />
+    </>
   );
-}
+};
