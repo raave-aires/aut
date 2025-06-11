6e01e8a3f97a0958b0d12f54e05d23102675a9ac
Raave Aires <dev@raave.me>
2025-06-10 22:27:21 -0300

    feat: instancia o db

---
+++ lib/db.ts
@@ -0,0 +1,16 @@
+// instância do banco de dados
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const db = globalThis.prisma || new PrismaClient(); 
+// faz com que o hot reload não crie múltiplas instâncias do client
+
+if(process.env.NODE_ENV !== "production"){
+  globalThis.prisma = db;
+};
+
+export default db;
