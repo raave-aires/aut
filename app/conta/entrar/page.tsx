226a281247f50bc1a07d2d5b4f03db227e5aca4f
Raave Aires <dev@raave.me>
2025-06-10 22:26:37 -0300

    wip: remove o botão de login com chave de acesso

--- app/conta/entrar/page.tsx
+++ app/conta/entrar/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
           <AuthButtons />
 
           <LoginForm />
-          <PasskeyLoginButton />
         </CardContent>
 
         <AuthFooter auth_type="login" />
