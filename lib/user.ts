4d319e8367d0502a4afc6f67704672db58fbb19c
Raave Aires <dev@raave.me>
2025-06-12 11:54:08 -0300

    feat: adiciona função para encontrar usuário pelo e-mail com verificação de senha

---
+++ lib/user.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { compareSync } from "bcrypt-ts"
+import db from "./db"
+
+type User = {
+  email: string
+  name: string,
+  password?: string
+}
+
+export async function findUserByEmail(
+  email: string,
+  password: string 
+): Promise<User | null> {
+  const user = await db.user.findFirst({
+    where: {
+      email: email,
+    }
+  });
+
+    if(!user){
+      return null;
+    };
+
+  const passwordMatch = compareSync(password, user.password);
+
+    if(passwordMatch){
+      return { email: user.email, name: ( user.name + " " +  user.lastname )}
+    }
+
+  return null;
+};
