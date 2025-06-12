145c55c5553423570b78aa04cc8b906525808b94
Raave Aires <dev@raave.me>
2025-06-12 11:54:41 -0300

    refactor: reorganiza provedores de autenticação e remove o provedor de magic links

--- auth.ts
+++ auth.ts
@@ -1,19 +1,47 @@
 // dependências:
 import NextAuth from "next-auth";
 
-// provedores OAuth:
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-
-// provedor de magic links:
-import Resend from "next-auth/providers/resend"
-
 // conector do banco de dados:
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
 
+// fornecedores:
+import type { Provider } from "next-auth/providers";
+import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+// ações:
+import { findUserByEmail } from "@/lib/user";
+
+const providers: Provider[] = [
+  Credentials({
+    credentials: {
+      email: {},
+      password: {},
+    },
+
+    authorize: async (credentials) => {
+      console.log(credentials);
+
+      // lógica de autenticação:
+      const user = await findUserByEmail(
+        credentials.email as string,
+        credentials.password as string
+      );
+
+      console.log(user)
+      return user;
+    },
+  }),
+  GitHub,
+  Google,
+];
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [ GitHub, Google, Resend ],
-  experimental: { enableWebAuthn: true },
+  providers,
+  // pages: {
+  //   signIn: "/conta/entrar",
+  // },
 });
