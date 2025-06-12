108528ead3df54280767dfd2c6a4229848e71d21
Raave Aires <dev@raave.me>
2025-06-12 11:56:57 -0300

    feat: adiciona componente SessionBanner para exibir dados de sessão

---
+++ components/auth/session.tsx
@@ -0,0 +1,14 @@
+import { auth } from "@/auth";
+
+export async function SessionBanner(){
+  const session = await auth();
+
+  return(
+    <div className="text-xs text-muted-foreground my-4">
+      <p>Dados de sessão</p>
+      <pre>
+        {JSON.stringify(session)}
+      </pre>
+    </div>
+  );
+};
