3fea4c08ce1119fbecafcb14e4a6eab2baad6b09
Raave Aires <dev@raave.me>
2025-06-12 11:54:57 -0300

    feat: implementa feedback visual e tratamento de erros no formulário de login

--- components/auth/forms/login-form.tsx
+++ components/auth/forms/login-form.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React, {
+  useState,
+  useActionState,
+  startTransition,
+  useEffect,
+} from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 // componentes:
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -19,20 +26,35 @@ import {
 import { Input } from "@/components/ui/input";
 import { Loader } from "@/components/auth/loader";
 
-// ícones: 
-import { EyeIcon, EyeClosedIcon } from "lucide-react";
+// ícones:
+import { AlertCircleIcon, Check, EyeIcon, EyeClosedIcon } from "lucide-react";
+
+//ações:
+import loginAction from "@/lib/loginAction";
 
 // esquema do zod:
 const loginInfos = z.object({
-  email: z.string()
-  .min(1, { message: "Precisamos de um e-mail ou nome de usuário" })
-  .email({ message: "O e-mail digitado não é válido"}),
+  email: z
+    .string()
+    .min(1, { message: "Precisamos de um e-mail ou nome de usuário" })
+    .email({ message: "O e-mail digitado não é válido" }),
   password: z.string(),
 });
 
 export function LoginForm() {
-  const [loading, setLoading] = useState(false);
-  
+  const router = useRouter();
+
+  const [state, formAction, isPending] = useActionState(loginAction, null);
+  const [showCheck, setShowCheck] = useState(false);
+  const [showErrorFlash, setShowErrorFlash] = useState(false);
+
+  // escolhe a classe de cor do botão
+  const buttonColorClass = showCheck
+    ? "bg-green-500"
+    : showErrorFlash
+    ? "bg-red-500 hover:bg-red-600"
+    : null;
+
   const form = useForm<z.infer<typeof loginInfos>>({
     resolver: zodResolver(loginInfos),
     defaultValues: {
@@ -46,16 +68,20 @@ export function LoginForm() {
   const disableShowPassButton = pass === "" || pass === undefined;
 
   async function onSubmit(values: z.infer<typeof loginInfos>) {
-    setLoading(true);
-    try {
-      // simula o login (troca pelo teu request real)
-      await new Promise((r) => setTimeout(r, 1200));
-      // aqui vai a lógica do login real, tipo: await signInComApi(values)
-      // redirect, etc
-    } finally {
-      setLoading(false);
-    }
+    startTransition(() => {
+      formAction(values);
+    });
   }
+
+  useEffect(() => {
+    if (state?.success) {
+      setShowCheck(true);
+      setTimeout(()=> console.log(""), 1000);
+    } else if (state?.success === false) {
+      setShowErrorFlash(true);
+      setTimeout(() => setShowErrorFlash(false), 2000);
+    }
+  }, [state?.success, router]);
 
   return (
     <Form {...form}>
@@ -93,15 +119,13 @@ export function LoginForm() {
                   className="cursor-pointer text-muted-foreground hover:text-black dark:hover:text-white p-0 h-3.5"
                   asChild
                 >
-                  <Link href="/conta/esqueci-a-senha">
-                    Esqueceu sua senha?
-                  </Link>
+                  <Link href="/conta/esqueci-a-senha">Esqueceu sua senha?</Link>
                 </Button>
               </FormLabel>
               <FormControl>
                 <div className="flex">
                   <Input
-                    type={ showPass ? "text" : "password" }
+                    type={showPass ? "text" : "password"}
                     placeholder="**********"
                     autoComplete="password"
                     className="rounded-r-none"
@@ -114,13 +138,13 @@ export function LoginForm() {
                     onClick={() => setShowPass((prev) => !prev)}
                     disabled={disableShowPassButton}
                   >
-                    { showPass && !disableShowPassButton ? (
+                    {showPass && !disableShowPassButton ? (
                       <EyeIcon className="h-4 w-4" aria-hidden="true" />
                     ) : (
                       <EyeClosedIcon className="h-4 w-4" aria-hidden="true" />
                     )}
                     <span className="sr-only">
-                      { showPass ? "Esconder senha" : "Mostrar senha"}
+                      {showPass ? "Esconder senha" : "Mostrar senha"}
                     </span>
                   </Button>
                 </div>
@@ -130,16 +154,26 @@ export function LoginForm() {
           )}
         />
 
-        <Button 
-          className="text-white"
-          type="submit" 
-          disabled={loading}
+        {state?.success === false ? (
+          <Alert variant="destructive">
+            <AlertCircleIcon />
+            <AlertTitle className="text-sm">Falha ao entrar.</AlertTitle>
+            <AlertDescription>
+              <p className="text-xs">
+                {state?.message ?? "Tente novamente mais tarde."}
+              </p>
+            </AlertDescription>
+          </Alert>
+        ) : state?.success === undefined ? null : null}
+
+        <Button
+          type="submit"
+          className={`text-white ${buttonColorClass}`}
+          disabled={isPending}
         >
-          { loading ? (
-            <Loader />
-          ) : "Entrar" }
+          {isPending ? <Loader /> : showCheck ? <Check size={16} /> : "Entrar"}
         </Button>
       </form>
     </Form>
   );
-};
+}
