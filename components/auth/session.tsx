80bc76bf90f6ef56673f9b71d757eee1b8806fd8
Raave Aires <dev@raave.me>
2025-06-13 10:46:57 -0300

    feat: formata a exibição dos dados de sessão no componente SessionBanner

--- components/auth/session.tsx
+++ components/auth/session.tsx
@@ -1,13 +1,13 @@
 import { auth } from "@/auth";
 
-export async function SessionBanner(){
+export async function SessionBanner() {
   const session = await auth();
 
-  return(
+  return (
     <div className="text-xs text-muted-foreground my-4">
       <p>Dados de sessão</p>
       <pre>
-        {JSON.stringify(session)}
+        {JSON.stringify(session, null, 2)}
       </pre>
     </div>
   );
