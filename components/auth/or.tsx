95c152770ca362f44c8d6257c9fcc88214d3bbaf
Raave Aires <dev@raave.me>
2025-06-13 15:56:13 -0300

    feat: adiciona componente Or para separador de opções na autenticação

---
+++ components/auth/or.tsx
@@ -0,0 +1,7 @@
+export function Or() {
+  return (
+    <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border">
+      ou
+    </p>
+  );
+}
